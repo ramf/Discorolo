0abd6c0588dad8b2ea8394979b9a3af2d9c4ee5d
Joao Pinto <joaopinto92@live.com>
2018-02-07 16:29:13 -0300

    Commit Final

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,6 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
+         validates :name, :email, :role, :password, presence: true
 
 end
